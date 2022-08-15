2cc11b792eb615c0361dec7b8932cd41cde3e9c4
krzm <kmazanek@gmail.com>
2022-08-16 00:58:06 +0200

    Add game particle example

--- Game/script7.js
+++ Game/script7.js
@@ -39,6 +39,7 @@ class Raven {
             , Math.floor(Math.random() * 255), Math.floor(Math.random() * 255),];
         this.color = 'rgb(' + this.randomColors[0] + ', ' + this.randomColors[1]
             + ', ' + this.randomColors[2] + ')';
+        this.hasTrail = Math.random() > 0.5;
     }
     update(deltatime) {
         if (this.y < 0 || this.y > canvas.height - this.height) {
@@ -52,6 +53,14 @@ class Raven {
             if (this.frame > this.maxFrame) this.frame = 0;
             else this.frame++;
             this.timeSinceFlap = 0;
+            if (this.hasTrail)
+            {
+                for (let i = 0; i < 5; i++)
+                {
+                    particles.push(
+                        new Particle(this.x, this.y, this.width, this.color));
+                }
+            }
         }
         if (this.x < 0 - this.width) gameOver = true;
     }
@@ -96,10 +105,11 @@ class Explosion {
 }
 
 let particles = [];
-class Particles {
+class Particle {
     constructor(x, y, size, color){
-        this.x = x;
-        this.y = y;
+        this.size = size;
+        this.x = x + this.size/2 + Math.random() * 50 - 25;
+        this.y = y + this.size/3 + Math.random() * 50 - 25;
         this.radius = Math.random() * this.size/10;
         this.maxRadius = Math.random() * 20 + 35;
         this.markForDeletion = false;
@@ -108,8 +118,17 @@ class Particles {
     }
     update(){
         this.x += this.speedX;
-        this.radius += 0.2;
-        if (this.radius > this.maxRadius) this.markForDeletion = true;
+        this.radius += 0.3;
+        if (this.radius > this.maxRadius - 5) this.markForDeletion = true;
+    }
+    draw(){
+        ctx.save();
+        ctx.globalAlpha = 1 - this.radius/this.maxRadius;
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.fill();
+        ctx.restore();
     }
 }
 
@@ -156,10 +175,11 @@ function animate(timestamp) {
         })
     }
     drawScore();
-    [...ravens, ...explosions].forEach(object => object.update(deltatime));
-    [...ravens, ...explosions].forEach(object => object.draw());
+    [...particles, ...ravens, ...explosions].forEach(object => object.update(deltatime));
+    [...particles, ...ravens, ...explosions].forEach(object => object.draw());
     ravens = ravens.filter(object => !object.markForDeletion);
     explosions = explosions.filter(object => !object.markForDeletion);
+    particles = particles.filter(object => !object.markForDeletion);
     if (!gameOver) requestAnimationFrame(animate);
     else drawGameOver();
 }
